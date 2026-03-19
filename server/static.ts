67308a71db25cb2a723f35f21e712efa16df00c7
rikigohain0028 <rikigohain0028@gmail.com>
2026-03-19 20:07:37 +0530

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -1,7 +1,10 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import express, { type Express } from "express";
 import fs from "fs";
-import path from "path";
-
 export function serveStatic(app: Express) {
   const distPath = path.resolve(__dirname, "public");
   if (!fs.existsSync(distPath)) {
